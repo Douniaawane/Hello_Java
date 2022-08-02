25d04d4628b9c13208f1281584b98e075919b13f
Douniaawane <102249747+Douniaawane@users.noreply.github.com>
2022-08-02 20:02:12 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Test {
 
-static void main(String[] args){
+public static void main(String[] args){
 
-	System.out.println("Hello World");
+    for(String s : args){
+	System.out.println(s);
+    }
 	
-}
+    }
 }
